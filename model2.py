72d1f3061b8f81d23dcbbd421f6751ac5840359d
Alexander Hoang <alexanderhoang4@gmail.com>
2023-11-29 23:20:11 -0800

    added comments

--- model2.py
+++ model2.py
@@ -18,6 +18,8 @@ batch_size = 32
 img_height = 200
 img_width = 200
 
+# used 80% images for training and 20% for validation
+
 train_ds = tf.keras.utils.image_dataset_from_directory(
   training_data_dir,
   validation_split=0.2,
@@ -34,27 +36,38 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+
+## class_names correspond to the directory names in alphabetical order
 class_names = train_ds.class_names
 print(class_names)
 
-for image_batch, labels_batch in train_ds:
-  print(image_batch.shape)
-  print(labels_batch.shape)
-  break
+## Everything below here to is train a model that is saved to my_model.hdf5
+## since we have already trained a model, this bottom half is commented out
 
-AUTOTUNE = tf.data.AUTOTUNE
+# for image_batch, labels_batch in train_ds:
+#   print(image_batch.shape)
+#   print(labels_batch.shape)
+#   break
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+## use buffered prefetching, so you can yield data from disk without having I/O become blocking
+## dataset.cashe keeps images in memeory after theyre loaded off disk during first epoch
+## dataset.prefetch overlaps data preprocessing and model execution while training
+# AUTOTUNE = tf.data.AUTOTUNE
 
-normalization_layer = layers.Rescaling(1./255)
-normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
-image_batch, labels_batch = next(iter(normalized_ds))
-first_image = image_batch[0]
-# Notice the pixel values are now in `[0,1]`.
-print(np.min(first_image), np.max(first_image))
+# train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+# val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-num_classes = len(class_names)
+## normalized the RGB channel from [0,255] to [0,1]
+# normalization_layer = layers.Rescaling(1./255)
+# normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+# image_batch, labels_batch = next(iter(normalized_ds))
+# first_image = image_batch[0]
+# # Notice the pixel values are now in `[0,1]`.
+# print(np.min(first_image), np.max(first_image))
+
+## we then have 3 convoulition bloicks and a max pooling layer in each of them
+## using ReLU as the activation function
+# num_classes = len(class_names)
 
 # model = Sequential([
 #   layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
@@ -69,6 +82,7 @@ num_classes = len(class_names)
 #   layers.Dense(num_classes)
 # ])
 
+## view the training and validation accuracy foor each training epoch by using model.compile
 # model.compile(optimizer='adam',
 #               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
 #               metrics=['accuracy'])
@@ -106,6 +120,8 @@ num_classes = len(class_names)
 # plt.title('Training and Validation Loss')
 # plt.show()
 
+## augment the data using random transformations to yield believable looking images to expose the model
+## to more aspects of the data and generalize better
 # data_augmentation = keras.Sequential(
 #   [
 #     layers.RandomFlip("horizontal",
@@ -117,6 +133,7 @@ num_classes = len(class_names)
 #   ]
 # )
 
+## used dropout to reduce overfitting 
 # model = Sequential([
 #   data_augmentation,
 #   layers.Rescaling(1./255),
